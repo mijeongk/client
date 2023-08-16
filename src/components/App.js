bee0f18c67b37f341a3aea7ef080278961530a59
mijeongk <rkdalwjd17@gmail.com>
2023-08-16 10:15:25 +0900

    230816

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,16 @@ import reactRouter2 from './R89_router2';
 import '../css/new.css';
 import Header from './Header/Header admin';
 import Footer from './Footer/Footer';
+import LoginForm from './LoginForm';
 
 class App extends Component {
   render(){
     return (
       <div>
         <Header/>
-        <Route exact path='/' component={reactRouter}/>
-        <Route exact path='/reactRouter2' component={reactRouter2}/>
+        {/* <Route exact path='/' component={reactRouter}/>
+        <Route exact path='/reactRouter2' component={reactRouter2}/> */}
+        <Route exact path='/' component={LoginForm}/>
         <Footer/>
       </div>
     );
